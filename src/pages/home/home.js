d6304a4d0adbdb6f1ec67b901757e3926d5dddef
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-03-07 16:22:57 -0800

    added social links! Sexy!

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,11 @@
 import BackgroundPhoto from '../../assets/gradienta-ix_kUDzCczo-unsplash.jpg'
+import TwitterPhoto from '../../assets/iconfinder_Circled_Twitter_svg_5279123.png'
+import FacebookPhoto from '../../assets/iconfinder_Circled_Facebook_svg_5279111.png'
+import YoutuePhoto from '../../assets/iconfinder_Circled_Youtube_svg_5279120.png'
+import InstagramPhoto from '../../assets/iconfinder_Circled_Instagram_svg_5279112.png'
+import LinkedinPhoto from '../../assets/iconfinder_Circled_Linkedin_svg_5279114.png'
+import GithubPhoto from '../../assets/iconfinder_github_rounded_394187.png'
+import DiscordPhoto from '../../assets/iconfinder_discord_4691356.png'
 import './home.css'
 import { useSpring, animated } from 'react-spring'
 import { Button } from 'reactstrap';
@@ -18,6 +25,17 @@ function HomeDisplay() {
         <hr></hr>
         <Button color='success' href="/signUp" style={{ marginTop: '20px', fontSize: '25px' }}>Sign Up</Button>
         <Button color='info' href="/signIn" style={{ marginTop: '20px', fontSize: '25px' }}>Sign In</Button>
+        <br></br>
+        <div class="row justify-content-md-center">
+          <img src={TwitterPhoto} alt="twitter.png" class="socialPhoto"></img>
+          <img src={InstagramPhoto} alt="instagram.png" class="socialPhoto"></img>
+          <img src={FacebookPhoto} alt="facebook.png" class="socialPhoto"></img>
+          <img src={LinkedinPhoto} alt="linkedin.png" class="socialPhoto"></img>
+          <img src={YoutuePhoto} alt="youtube.png" class="socialPhoto"></img>
+          <img src={GithubPhoto} alt="discord.png" class="socialPhoto"></img>
+
+        </div>
+
       </animated.div>
     </div>
   );

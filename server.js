2a804fe9bc816fddb995bb3c416c55858ec3cb27
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-03-25 07:09:15 -0700

    connected mongoDB to the server.js, and built new database to hold and serve user data

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/1on1UserData', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const connection = mongoose.connection
+
+connection.once('open', function () {
+  console.log('MongoDB database conection established successfully');
+});
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -9,3 +25,4 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+

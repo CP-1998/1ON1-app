5af670e5d5306335f20df66c5307fdb3364e3ebb
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-03-27 08:33:55 -0700

    working out the backend

--- back-end/userData.model.js
+++ back-end/userData.model.js
@@ -1,2 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+let OneonOneUserData = new Schema({
+  OneonOneUserData_type: {
+    type: String
+  },
+
+  OneonOneUserData_premium: {
+    type: Boolean
+  },
+
+  OneonOneUserData_online: {
+    type: Boolean
+  },
+
+  OneonOneUserData_messages: {
+    type: String
+  }
+});
+
+let userData = new mongoose.model("UserData", OneonOneUserData)
+
+module.exports = userData;
--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const OneonOneUserDataRoutes = express.Router();
+const userData = require('./back-end/userData.model.js')
 
+app.use('/userData', OneonOneUserDataRoutes)
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-mongoose.connect('mongodb://127.0.0.1:27017/1on1UserData', {
+mongoose.connect('mongodb://127.0.0.1:27017/OneonOneUserData', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -25,4 +30,26 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+app.post('/userTypeSelect', (req, res) => {
+  let userTypeData = new userData(req.body)
+  userTypeData.save()
+    .then(item => {
+      res.send("item saved to database")
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database :(")
+    });
+});
+
+OneonOneUserDataRoutes.route('/').get(function (req, res) {
+  userData.find(function (err, userData) {
+    if (err) {
+      return console.log(err);
+    } else {
+      return res.json(userData);
+    }
+  })
+})
+
 
